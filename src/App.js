7552d7271f8a7b8f51fd45024307be10cd5e8bda
S2ej1n <ks320205@naver.com>
2024-12-04 16:04:38 +0900

    클래스 컴포넌트로 작성해보기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+// 리액트 라이브러리에서 컴포넌트 가져옴
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// 컴포넌트를 사용할 수 있게 extends 확장해줌
+export default class App extends Component {
+  // 컴포넌트가 있으면 render 메소드 사용 가능
+  render() {
+    return(
+      // 리턴 안에 유아이 작성
+      <div>
+        안녕하세요.
+      </div>
+    )
+  }
 }
-
-export default App;
