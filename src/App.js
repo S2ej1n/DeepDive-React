3674ac385eca4b20a15f2d9ad5df108c6e251c1f
S2ej1n <ks320205@naver.com>
2024-12-09 01:15:30 +0900

    tailwind css 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import List from "./components/List";
+import Form from "./components/Form";
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value)
-  }
 
   const handleSubmit = (e) => {
     // form 안에 input을 전송할 때 페이지가 리로드 되는 것을 막아줌
@@ -34,25 +31,12 @@ export default function App() {
         <div className="title">
           <h1> 할 일 목록 </h1>
         </div>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
 
         <List todoData={todoData} setTodoData={setTodoData}/>
-
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: '10', padding: '5px' }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: '1' }}
-          />
-        </form>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue}/>
       </div>
     </div>
   );
