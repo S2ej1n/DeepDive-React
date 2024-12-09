70dde98b4928a1b7a3c92fd6d21054f4b9635de0
S2ej1n <ks320205@naver.com>
2024-12-09 19:16:18 +0900

    할일 수정하기

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = React.memo(({
     id, title, completed, todoData, 
@@ -6,6 +6,9 @@ const List = React.memo(({
     handleClick
 }) => {
     console.log('List render');
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState();
 
     const handleCompleteChange = (id) => {
         let newTodoData = todoData.map(data => {
@@ -18,25 +21,75 @@ const List = React.memo(({
         setTodoData(newTodoData);
     };
 
-    return (
-        <div key={id}
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-            {...provided.dragHandleProps}
-            className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} 
-                                    flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
+    const handleEditChange = (event) => {
+        setEditedTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+            let newTodoData = todoData.map((data) => {
+                if (data.id === id) {
+                    data.title = editedTitle;
+                }
+                return data;
+            });
+            setTodoData(newTodoData);
+            setIsEditing(false);
+    }
+
+    if (isEditing) {
+        return (<div
+            className={`flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
             <div className='items-center'>
-                <input type="checkbox" defaultChecked={false}
-                    onChange={() => handleCompleteChange(id)} />
-                <span className={completed ? 'line-through' : undefined}>{title}</span>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        value={editedTitle}
+                        onChange={handleEditChange}
+                        className='w-full px-3 py-2 mr-4 text-gray-500 rounded' />
+                </form>
             </div>
             <div className='items-center'>
                 <button
                     className='px-4 py-2 float-right'
-                    onClick={() => handleClick(id)}>X</button>
+                    onClick={() => setIsEditing(false)}
+                >X
+                </button>
+                <button
+                    onClick={handleSubmit}
+                    className='px-4 py-2 float-right'
+                    type='submit'
+                >save
+                </button>
             </div>
-        </div>
-    )
+        </div>)
+    } else {
+        return (
+            <div key={id}
+                {...provided.draggableProps}
+                ref={provided.innerRef}
+                {...provided.dragHandleProps}
+                className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} 
+                                        flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
+                <div className='items-center'>
+                    <input type="checkbox" defaultChecked={false}
+                        onChange={() => handleCompleteChange(id)} />
+                    <span className={completed ? 'line-through' : undefined}>{title}</span>
+                </div>
+                <div className='items-center'>
+                    <button
+                        className='px-4 py-2 float-right'
+                        onClick={() => handleClick(id)}
+                    >X
+                    </button>
+                    <button
+                        className='px-4 py-2 float-right'
+                        onClick={() => setIsEditing(true)}
+                    >edit
+                    </button>
+                </div>
+            </div>
+        )
+    }
 });
 
 export default List
